ad5482227d726786debf0153be7ac05537b0a5ff
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-04 15:39:37 -0500

    add ins01Description add ins02Relationship add ins03TransactionTypeCode add ins04MaintenanceReasonCode add ins05BenefitStatusCode

--- src/app/feature/edi-viewer/ins/ins.ts
+++ src/app/feature/edi-viewer/ins/ins.ts
@@ -16,6 +16,31 @@ export class Ins implements OnInit {
   insSegmentDelimiter = input<string>('~');
   insValid = false;
   ins: string[] | undefined;
+  ins01Description = [
+    { code: 'Y', description: 'subscriber' },
+    { code: 'N', description: 'dependent' }
+  ];
+  ins02Relationship = [
+    { code: '01', description: 'child' },
+    { code: '02', description: 'spouse' },
+    { code: '03', description: 'dependent' },
+    { code: '04', description: 'parent' },
+    { code: '05', description: 'grandparent' },
+    { code: '06', description: 'sibling' },
+    { code: '07', description: 'other family member' },
+    { code: '18', description: 'self' }
+  ];
+  ins03TransactionTypeCode = [
+    { code: '021', description: 'add' },
+    { code: '024', description: 'cancellation' }
+  ];
+  ins04MaintenanceReasonCode = [
+    { code: 'XN', description: 'new enrollment' }
+  ];
+  ins05BenefitStatusCode = [
+    { code: 'A', description: 'active' },
+    { code: 'T', description: 'terminated' }
+  ]
 
   ngOnInit() {
     const insSegmentLength = this.insData().length;
@@ -34,5 +59,25 @@ export class Ins implements OnInit {
 
   getInsElement(index: number) {
     return this.ins![index];
+  }
+
+  getIns01Description(code: string) {
+    return this.ins01Description.find(x => x.code === code);
+  }
+
+  getIns02Relationship(code: string) {
+    return this.ins02Relationship.find(x => x.code === code);
+  }
+
+  getIns03TransactionTypeCode(code: string) {
+    return this.ins03TransactionTypeCode.find(x => x.code === code);
+  }
+
+  getIns04MaintenaceReasonCode(code: string) {
+    return this.ins04MaintenanceReasonCode.find(x => x.code === code);
+  }
+
+  getIns05BenefitStatusCode(code: string) {
+    return this.ins05BenefitStatusCode.find(x => x.code === code);
   }
 }
