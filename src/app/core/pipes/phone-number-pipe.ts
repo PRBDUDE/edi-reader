22f2a234d711a55f0118a151438faf6055900e02
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-05 12:27:44 -0500

    add phone formats

--- src/app/core/pipes/phone-number-pipe.ts
+++ src/app/core/pipes/phone-number-pipe.ts
@@ -7,8 +7,14 @@ export class PhoneNumberPipe implements PipeTransform {
 
   transform(value: string | number): string {
     const phoneNumber = value?.toString();
+    if (phoneNumber?.length === 7) {
+      return `${phoneNumber.slice(0, 3)}-${phoneNumber.slice(3)}`;
+    }
     if (phoneNumber?.length === 10) {
       return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6)}`;
+    }
+    if (phoneNumber?.length >= 11) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6,10)} ext: ${phoneNumber.slice(10)}`
     }
     return phoneNumber;
   }
