50d7a0a02fab96e7475286649cfdaa22974f5b2e
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-05 12:18:16 -0500

    add beforeEach

--- src/app/core/pipes/phone-number-pipe.spec.ts
+++ src/app/core/pipes/phone-number-pipe.spec.ts
@@ -1,8 +1,17 @@
-import { PhoneNumberPipe } from './phone-number-pipe';
+import {PhoneNumberPipe} from './phone-number-pipe';
 
 describe('PhoneNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PhoneNumberPipe();
+  let pipe: PhoneNumberPipe;
+
+  beforeEach(() => {
+    pipe = new PhoneNumberPipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should format \'2334445533\' as \'(233) 444-5533\'', () => {
+    expect(pipe.transform('2334445533')).toBe('(233) 444-5533');
   });
 });
