e2a57f239cdb7d45f8f8a5b2f30c8e5d0fd6a8bb
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-07 15:34:54 -0500

    add guard for null

--- src/app/core/pipes/d8-date-pipe.ts
+++ src/app/core/pipes/d8-date-pipe.ts
@@ -6,10 +6,12 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class D8DatePipe implements PipeTransform {
 
   transform(value: string): string {
+    if (!value) {
+      return '';
+    }
     if (value.length === 8) {
       return `${value.slice(4, 6)}/${value.slice(6)}/${value.slice(0, 4)}`;
     }
     return value;
   }
-
 }
