e35d80f096caeef5c011b0360fa0afd23ebc5f8e
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-10 15:09:23 -0500

    clean up code

--- src/app/core/edi/builders/ref-builder.spec.ts
+++ src/app/core/edi/builders/ref-builder.spec.ts
@@ -1,19 +1,26 @@
 import {RefBuilder} from './ref-builder';
 import {getTestEdiDelimiters} from '@ediTest/edi-utilities';
+import {Segment} from '@edi/segment';
 
 describe('RefBuilder', () => {
   let ref0FBuilder: RefBuilder;
   let ref38Builder: RefBuilder;
   let ref1LBuilder: RefBuilder;
+  let segment0F: Segment;
+  let segment38: Segment;
+  let segment1L: Segment;
   const ref0F = 'REF*0F*GROUP123~';
   const ref1L = 'REF*1L*GROUP123~';
   const ref38 = 'REF*38*GROUP123~';
   const delimiters = getTestEdiDelimiters();
 
   beforeEach(() => {
-    ref38Builder = new RefBuilder(ref38, delimiters);
     ref0FBuilder = new RefBuilder(ref0F, delimiters);
+    ref38Builder = new RefBuilder(ref38, delimiters);
     ref1LBuilder = new RefBuilder(ref1L, delimiters);
+    segment0F = ref0FBuilder.getSegment();
+    segment38 = ref38Builder.getSegment();
+    segment1L = ref1LBuilder.getSegment();
   });
 
   it('should create an instance', () => {
@@ -22,52 +29,52 @@ describe('RefBuilder', () => {
 
   describe('check elements', () => {
     it('should return \'REF\' in element 0', () => {
-      expect(ref38Builder.getSegment().getElement(0)).toEqual('REF');
+      expect(segment38.getElement(0)).toEqual('REF');
     });
 
     it('should return \'38\' in element 1', () => {
-      expect(ref38Builder.getSegment().getElement(1)).toEqual('38');
+      expect(segment38.getElement(1)).toEqual('38');
     });
 
     it('should return \'GROUP123\' in element 2', () => {
-      expect(ref38Builder.getSegment().getElement(2)).toEqual('GROUP123');
+      expect(segment38.getElement(2)).toEqual('GROUP123');
     });
   });
 
   describe('check descriptions', () => {
     describe('for REF*0F', () => {
       it('should display \'reference identifier code\' for REF01', () => {
-        expect(ref0FBuilder.getSegment().getSegmentId(1)).toEqual('REF01');
-        expect(ref0FBuilder.getSegment().getDescription(1)).toEqual('contract number');
+        expect(segment0F.getSegmentId(1)).toEqual('REF01');
+        expect(segment0F.getDescription(1)).toEqual('contract number');
       });
 
       it('should display \'reference identification\' for REF02', () => {
-        expect(ref0FBuilder.getSegment().getSegmentId(2)).toEqual('REF02');
-        expect(ref0FBuilder.getSegment().getDescription(2)).toEqual('contract number');
+        expect(segment0F.getSegmentId(2)).toEqual('REF02');
+        expect(segment0F.getDescription(2)).toEqual('contract number');
       });
     });
 
     describe('for REF*38', () => {
       it('should display \'reference identifier code\' for REF01', () => {
-        expect(ref38Builder.getSegment().getSegmentId(1)).toEqual('REF01');
-        expect(ref38Builder.getSegment().getDescription(1)).toEqual('group');
+        expect(segment38.getSegmentId(1)).toEqual('REF01');
+        expect(segment38.getDescription(1)).toEqual('group');
       });
 
       it('should display \'reference identification\' for REF02', () => {
-        expect(ref38Builder.getSegment().getSegmentId(2)).toEqual('REF02');
-        expect(ref38Builder.getSegment().getDescription(2)).toEqual('group number');
+        expect(segment38.getSegmentId(2)).toEqual('REF02');
+        expect(segment38.getDescription(2)).toEqual('group number');
       });
     });
 
     describe('for REF*1L', () => {
       it('should display \'reference identifier code\' for REF01', () => {
-        expect(ref1LBuilder.getSegment().getSegmentId(1)).toEqual('REF01');
-        expect(ref1LBuilder.getSegment().getDescription(1)).toEqual('sub plan');
+        expect(segment1L.getSegmentId(1)).toEqual('REF01');
+        expect(segment1L.getDescription(1)).toEqual('sub plan');
       });
 
       it('should display \'reference identification\' for REF02', () => {
-        expect(ref1LBuilder.getSegment().getSegmentId(2)).toEqual('REF02');
-        expect(ref1LBuilder.getSegment().getDescription(2)).toEqual('sub plan ID');
+        expect(segment1L.getSegmentId(2)).toEqual('REF02');
+        expect(segment1L.getDescription(2)).toEqual('sub plan ID');
       });
     });
   })
--- src/app/core/edi/builders/st-builder.spec.ts
+++ src/app/core/edi/builders/st-builder.spec.ts
@@ -1,13 +1,16 @@
 import { StBuilder } from './st-builder';
 import {getTestEdiDelimiters} from '@ediTest/edi-utilities';
+import {Segment} from '@edi/segment';
 
 describe('StBuilder', () => {
   let stBuilder: StBuilder;
+  let segment: Segment;
   const stSegment = 'ST*834*0001*005010X220A1~';
   const delimiters = getTestEdiDelimiters();
 
   beforeEach(() => {
     stBuilder = new StBuilder(stSegment, delimiters);
+    segment = stBuilder.getSegment();
   });
 
   it('should create an instance', () => {
@@ -16,36 +19,36 @@ describe('StBuilder', () => {
 
   describe('check elements', () => {
     it('should return \'ST\' in element 0', () => {
-      expect(stBuilder.getSegment().getElement(0)).toEqual('ST');
+      expect(segment.getElement(0)).toEqual('ST');
     });
 
     it('should return \'834\' in element 1', () => {
-      expect(stBuilder.getSegment().getElement(1)).toEqual('834');
+      expect(segment.getElement(1)).toEqual('834');
     });
 
     it('should return \'0001\' in element 2', () => {
-      expect(stBuilder.getSegment().getElement(2)).toEqual('0001');
+      expect(segment.getElement(2)).toEqual('0001');
     });
 
     it('should return \'005010X220A1\' in element 3', () => {
-      expect(stBuilder.getSegment().getElement(3)).toEqual('005010X220A1');
+      expect(segment.getElement(3)).toEqual('005010X220A1');
     });
   });
 
   describe('check descriptions', () => {
     it('should display \'transaction set identifier code\' for ST01', () => {
-      expect(stBuilder.getSegment().getSegmentId(1)).toEqual('ST01');
-      expect(stBuilder.getSegment().getDescription(1)).toEqual('transaction set identifier code');
+      expect(segment.getSegmentId(1)).toEqual('ST01');
+      expect(segment.getDescription(1)).toEqual('transaction set identifier code');
     });
 
     it('should display \'transaction set control number\' for ST02', () => {
-      expect(stBuilder.getSegment().getSegmentId(2)).toEqual('ST02');
-      expect(stBuilder.getSegment().getDescription(2)).toEqual('transaction set control number');
+      expect(segment.getSegmentId(2)).toEqual('ST02');
+      expect(segment.getDescription(2)).toEqual('transaction set control number');
     });
 
     it('should display \'implementation convention reference\' for ST03', () => {
-      expect(stBuilder.getSegment().getSegmentId(3)).toEqual('ST03');
-      expect(stBuilder.getSegment().getDescription(3)).toEqual('implementation convention reference');
+      expect(segment.getSegmentId(3)).toEqual('ST03');
+      expect(segment.getDescription(3)).toEqual('implementation convention reference');
     });
   })
 });
