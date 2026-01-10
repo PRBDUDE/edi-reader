64a0ea8cd1a540fb14dd5aaccdfc10fd885ab0c5
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-10 08:46:39 -0500

    add ge-builder

---
+++ src/app/core/edi/builders/ge-builder.ts
@@ -0,0 +1,13 @@
+import {EdiBuilder} from '@edi/builders/edi-builder';
+import {Delimiters} from '@edi/delimiters';
+
+export class GeBuilder extends EdiBuilder {
+  constructor(segment: string, delimiters: Delimiters) {
+    super(segment, delimiters);
+  }
+
+  override setDescriptions() {
+    this.setIdAndDescription(1, 'GE01', 'number of transaction sets');
+    this.setIdAndDescription(2, 'GE02', 'control group number (must match GS06)');
+  }
+}
