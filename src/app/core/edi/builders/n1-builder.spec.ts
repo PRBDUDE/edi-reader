c9cc6831929a6b53285aed111b12c81361d475ac
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-10 15:09:23 -0500

    clean up code

--- src/app/core/edi/builders/n1-builder.spec.ts
+++ src/app/core/edi/builders/n1-builder.spec.ts
@@ -5,6 +5,8 @@ import {Segment} from '@edi/segment';
 describe('N1Builder', () => {
   let n1P5Builder: N1Builder;
   let n1INBuilder: N1Builder;
+  let segmentP5: Segment;
+  let segmentIN: Segment;
   const n1P5Segment = 'N1*P5*ACME EMPLOYER~';
   const n1INSegment = 'N1*IN*ALPHA HEALTH PLAN~';
   const delimiters = getTestEdiDelimiters();
@@ -12,6 +14,8 @@ describe('N1Builder', () => {
   beforeEach(() => {
     n1P5Builder = new N1Builder(n1P5Segment, delimiters);
     n1INBuilder = new N1Builder(n1INSegment, delimiters);
+    segmentP5 = n1P5Builder.getSegment();
+    segmentIN = n1INBuilder.getSegment();
   })
 
   it('should create an instance', () => {
@@ -20,59 +24,45 @@ describe('N1Builder', () => {
   });
 
   describe('check elements', () => {
-    let segment: Segment;
-
     beforeEach(() => {
-      segment = n1P5Builder.getSegment();
+      segmentP5 = n1P5Builder.getSegment();
     })
 
     it('should return \'N1\' in element 0', () => {
-      expect(segment.getElement(0)).toEqual('N1');
+      expect(segmentP5.getElement(0)).toEqual('N1');
     });
 
     it('should return \'P5\' in element 1', () => {
-      expect(segment.getElement(1)).toEqual('P5');
+      expect(segmentP5.getElement(1)).toEqual('P5');
     });
 
     it('should return \'ACME EMPLOYER\' in element 2', () => {
-      expect(segment.getElement(2)).toEqual('ACME EMPLOYER');
+      expect(segmentP5.getElement(2)).toEqual('ACME EMPLOYER');
     });
   });
 
   describe('check descriptions', () => {
     describe('for N1*P5', () => {
-      let segment: Segment;
-
-      beforeEach(() => {
-        segment = n1P5Builder.getSegment();
-      });
-
       it('should display \'entity identifier code\' for N101', () => {
-        expect(segment.getSegmentId(1)).toEqual('N101');
-        expect(segment.getDescription(1)).toEqual('entity identifier code');
+        expect(segmentP5.getSegmentId(1)).toEqual('N101');
+        expect(segmentP5.getDescription(1)).toEqual('entity identifier code');
       });
 
       it('should display \'entity name\' for N102', () => {
-        expect(segment.getSegmentId(2)).toEqual('N102');
-        expect(segment.getDescription(2)).toEqual('identification code qualifier');
+        expect(segmentP5.getSegmentId(2)).toEqual('N102');
+        expect(segmentP5.getDescription(2)).toEqual('identification code qualifier');
       });
     });
 
     describe('for N1*IN', () => {
-      let segment: Segment;
-
-      beforeEach(() => {
-        segment = n1INBuilder.getSegment();
-      });
-
       it('should display \'entity identifier code\' for N101', () => {
-        expect(segment.getSegmentId(1)).toEqual('N101');
-        expect(segment.getDescription(1)).toEqual('entity identifier code');
+        expect(segmentIN.getSegmentId(1)).toEqual('N101');
+        expect(segmentIN.getDescription(1)).toEqual('entity identifier code');
       });
 
       it('should display \'entity name\' for N102', () => {
-        expect(segment.getSegmentId(2)).toEqual('N102');
-        expect(segment.getDescription(2)).toEqual('identification code qualifier');
+        expect(segmentIN.getSegmentId(2)).toEqual('N102');
+        expect(segmentIN.getDescription(2)).toEqual('identification code qualifier');
       })
     });
   });
--- src/app/core/edi/builders/se-builder.spec.ts
+++ src/app/core/edi/builders/se-builder.spec.ts
@@ -1,13 +1,16 @@
 import { SeBuilder } from './se-builder';
 import {getTestEdiDelimiters} from '@ediTest/edi-utilities';
+import {Segment} from '@edi/segment';
 
 describe('SeBuilder', () => {
   let seBuilder: SeBuilder;
+  let segment: Segment;
   const seSegment = 'SE*10*0001~';
   const delimiters = getTestEdiDelimiters();
 
   beforeEach(() => {
     seBuilder = new SeBuilder(seSegment, delimiters);
+    segment = seBuilder.getSegment();
   });
 
   it('should create an instance', () => {
@@ -16,27 +19,27 @@ describe('SeBuilder', () => {
 
   describe('check elements', () => {
     it('should return \'SE\' in element 0', () => {
-      expect(seBuilder.getSegment().getElement(0)).toEqual('SE');
+      expect(segment.getElement(0)).toEqual('SE');
     });
 
     it('should return \'10\' in element 1', () => {
-      expect(seBuilder.getSegment().getElement(1)).toEqual('10');
+      expect(segment.getElement(1)).toEqual('10');
     });
 
     it('should return \'0001\' in element 2', () => {
-      expect(seBuilder.getSegment().getElement(2)).toEqual('0001');
+      expect(segment.getElement(2)).toEqual('0001');
     });
   });
 
   describe('check descriptions', () => {
     it('should display \'number of included segments\' for SE01', () => {
-      expect(seBuilder.getSegment().getSegmentId(1)).toEqual('SE01');
-      expect(seBuilder.getSegment().getDescription(1)).toEqual('segment count from ST-SE including ST & SE lines');
+      expect(segment.getSegmentId(1)).toEqual('SE01');
+      expect(segment.getDescription(1)).toEqual('segment count from ST-SE including ST & SE lines');
     });
 
     it('should display \'control number of the group\' for SE02', () => {
-      expect(seBuilder.getSegment().getSegmentId(2)).toEqual('SE02');
-      expect(seBuilder.getSegment().getDescription(2)).toEqual('transaction set control number');
+      expect(segment.getSegmentId(2)).toEqual('SE02');
+      expect(segment.getDescription(2)).toEqual('transaction set control number');
     });
   })
 });
