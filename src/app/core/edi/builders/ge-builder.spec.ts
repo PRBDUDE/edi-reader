6fc8350380a2904f11928665847d29610f3ba8f6
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-10 15:01:16 -0500

    clean up code

--- src/app/core/edi/builders/ge-builder.spec.ts
+++ src/app/core/edi/builders/ge-builder.spec.ts
@@ -1,13 +1,16 @@
 import {GeBuilder} from './ge-builder';
 import {getTestEdiDelimiters} from '@ediTest/edi-utilities';
+import {Segment} from '@edi/segment';
 
 describe('GeBuilder', () => {
   let geBuilder: GeBuilder;
+  let segment: Segment;
   const geSegment = 'GE*2*1~';
   const delimiters = getTestEdiDelimiters();
 
   beforeEach(() => {
     geBuilder = new GeBuilder(geSegment, delimiters);
+    segment = geBuilder.getSegment();
   });
 
   it('should create an instance', () => {
@@ -16,27 +19,27 @@ describe('GeBuilder', () => {
 
   describe('check elements', () => {
     it('should return \'GE\' in element 0', () => {
-      expect(geBuilder.getSegment().getElement(0)).toEqual('GE');
+      expect(segment.getElement(0)).toEqual('GE');
     });
 
     it('should return \'2\' in element 1', () => {
-      expect(geBuilder.getSegment().getElement(1)).toEqual('2');
+      expect(segment.getElement(1)).toEqual('2');
     });
 
     it('should return \'1\' in element 2', () => {
-      expect(geBuilder.getSegment().getElement(2)).toEqual('1');
+      expect(segment.getElement(2)).toEqual('1');
     });
   });
 
   describe('check descriptions', () => {
     it('should display \'number of transaction sets\' for GE01', () => {
-      expect(geBuilder.getSegment().getSegmentId(1)).toEqual('GE01');
-      expect(geBuilder.getSegment().getDescription(1)).toEqual('number of transaction sets');
+      expect(segment.getSegmentId(1)).toEqual('GE01');
+      expect(segment.getDescription(1)).toEqual('number of transaction sets');
     });
 
     it('should display \'control group number (must match GS06)\' for GE02', () => {
-      expect(geBuilder.getSegment().getSegmentId(2)).toEqual('GE02');
-      expect(geBuilder.getSegment().getDescription(2)).toEqual('control group number (must match GS06)');
+      expect(segment.getSegmentId(2)).toEqual('GE02');
+      expect(segment.getDescription(2)).toEqual('control group number (must match GS06)');
     });
   });
 });
--- src/app/core/edi/builders/gs-builder.spec.ts
+++ src/app/core/edi/builders/gs-builder.spec.ts
@@ -1,13 +1,16 @@
 import {GsBuilder} from './gs-builder';
 import {getTestEdiDelimiters} from '@ediTest/edi-utilities';
+import {Segment} from '@edi/segment';
 
 describe('GsBuilder', () => {
   let gsBuilder: GsBuilder;
+  let segment: Segment;
   const gsSegment = 'GS*BE*87790056*576687090*20251107*1430*1*X*005010X220A1~';
   const delimiters = getTestEdiDelimiters();
 
   beforeEach(() => {
     gsBuilder = new GsBuilder(gsSegment, delimiters);
+    segment = gsBuilder.getSegment();
   });
 
   it('should create an instance', () => {
@@ -16,73 +19,73 @@ describe('GsBuilder', () => {
 
   describe('check elements', () => {
     it('should return \'GS\' in element 0', () => {
-      expect(gsBuilder.getSegment().getElement(0)).toEqual('GS');
+      expect(segment.getElement(0)).toEqual('GS');
     });
 
     it('should return \'BE\' in element 1', () => {
-      expect(gsBuilder.getSegment().getElement(1)).toEqual('BE');
+      expect(segment.getElement(1)).toEqual('BE');
     });
 
     it('should return \'87790056\' in element 2', () => {
-      expect(gsBuilder.getSegment().getElement(2)).toEqual('87790056');
+      expect(segment.getElement(2)).toEqual('87790056');
     });
 
     it('should return \'576687090\' in element 3', () => {
-      expect(gsBuilder.getSegment().getElement(3)).toEqual('576687090');
+      expect(segment.getElement(3)).toEqual('576687090');
     });
 
     it('should return \'20251107\' in element 4', () => {
-      expect(gsBuilder.getSegment().getElement(4)).toEqual('20251107');
+      expect(segment.getElement(4)).toEqual('20251107');
     });
 
     it('should return \'1430\' in element 5', () => {
-      expect(gsBuilder.getSegment().getElement(5)).toEqual('1430');
+      expect(segment.getElement(5)).toEqual('1430');
     });
 
     it('should return \'1\' in element 6', () => {
-      expect(gsBuilder.getSegment().getElement(6)).toEqual('1');
+      expect(segment.getElement(6)).toEqual('1');
     });
 
     it('should return \'X\' in element 7', () => {
-      expect(gsBuilder.getSegment().getElement(7)).toEqual('X');
+      expect(segment.getElement(7)).toEqual('X');
     });
 
     it('should return \'005010X220A1\' in element 8', () => {
-      expect(gsBuilder.getSegment().getElement(8)).toEqual('005010X220A1');
+      expect(segment.getElement(8)).toEqual('005010X220A1');
     });
   });
 
   describe('check descriptions', () => {
     it('should display \'functional identifier code\' for GS01', () => {
-      expect(gsBuilder.getSegment().getDescription(1)).toEqual('functional identifier code');
+      expect(segment.getDescription(1)).toEqual('functional identifier code');
     });
 
     it('should display \'application senders code\' for GS02', () => {
-      expect(gsBuilder.getSegment().getDescription(2)).toEqual('application senders code');
+      expect(segment.getDescription(2)).toEqual('application senders code');
     });
 
     it('should display \'application receivers code\' for GS03', () => {
-      expect(gsBuilder.getSegment().getDescription(3)).toEqual('application receivers code');
+      expect(segment.getDescription(3)).toEqual('application receivers code');
     });
 
     it('should display \'date\' for GS04', () => {
-      expect(gsBuilder.getSegment().getDescription(4)).toEqual('date the group was built (YYYYMMDD)');
+      expect(segment.getDescription(4)).toEqual('date the group was built (YYYYMMDD)');
     });
 
     it('should display \'time the group was built (HHMM)\' for GS05', () => {
-      expect(gsBuilder.getSegment().getDescription(5)).toEqual('time the group was built (HHMM)');
+      expect(segment.getDescription(5)).toEqual('time the group was built (HHMM)');
     });
 
     it('should display \'control number\' for GS06', () => {
-      expect(gsBuilder.getSegment().getDescription(6)).toEqual('control number');
+      expect(segment.getDescription(6)).toEqual('control number');
     });
 
     it('should display \'responsible agency code\' for GS07', () => {
-      expect(gsBuilder.getSegment().getDescription(7)).toEqual('responsible agency code');
+      expect(segment.getDescription(7)).toEqual('responsible agency code');
     });
 
     it('should display \'version release / inquiry\' for GS08', () => {
-      expect(gsBuilder.getSegment().getDescription(8)).toEqual('version release / inquiry');
+      expect(segment.getDescription(8)).toEqual('version release / inquiry');
     });
   });
 });
