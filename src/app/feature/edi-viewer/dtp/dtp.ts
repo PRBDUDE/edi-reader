e35cb2791b765fe39077064909059dc7661c4c63
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-11 15:21:09 -0500

    move dtp descriptions to separate file

--- src/app/feature/edi-viewer/dtp/dtp.ts
+++ src/app/feature/edi-viewer/dtp/dtp.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {ElementDescription} from '../element-description/element-description';
 import {D8DatePipe} from '@pipes/d8-date-pipe';
 import {Segment} from '../segment/segment';
+import {getDtpDateType, getDtpFormat} from '@edi/builders/code-descriptions/dtp';
 
 @Component({
   selector: 'prb-dtp',
@@ -13,21 +14,6 @@ import {Segment} from '../segment/segment';
   styleUrls: ['./dtp.scss', '../edi-viewer.scss']
 })
 export class Dtp extends Segment {
-  dtp01DateType = [
-    {code: '001', description: 'cancel after date'},
-    {code: '007', description: 'effective date'},
-    {code: '011', description: 'ship date'},
-    {code: '336', description: 'employment begin date'},
-    {code: '346', description: 'plan issue date'},
-    {code: '348', description: 'benefit start date'},
-    {code: '349', description: 'benefit end date'},
-  ]
-  dtp02Format = [
-    {code: 'D6', description: 'YYMMDD'},
-    {code: 'D8', description: 'YYYYMMDD'},
-    {code: 'DT', description: 'YYYYMMDDHHMM'},
-  ]
-
   constructor() {
     super();
   }
@@ -37,11 +23,6 @@ export class Dtp extends Segment {
     this.valid = true;
   }
 
-  getDtpDateType(code: string) {
-    return this.dtp01DateType.find(x => x.code === code);
-  }
-
-  getDtpFormat(code: string) {
-    return this.dtp02Format.find(x => x.code === code);
-  }
+  protected readonly getDtp01DateType = getDtpDateType;
+  protected readonly getDtp02Format = getDtpFormat;
 }
