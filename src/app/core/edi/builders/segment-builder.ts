5f0468486a8f59a0745188556925d72b0005eb69
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-13 06:15:06 -0500

    add constructor and pass ISA segment

--- src/app/core/edi/builders/segment-builder.ts
+++ src/app/core/edi/builders/segment-builder.ts
@@ -14,21 +14,27 @@ import {N4Builder} from '@edi/builders/n4-builder';
 import {InsBuilder} from '@edi/builders/ins-builder';
 
 export class SegmentBuilder {
-  private delimiters: Delimiters | undefined;
+  private delimiters: Delimiters;
+
+  constructor(ediData: string) {
+    const isa = ediData.substring(0, 104);
+    if (isa.startsWith('ISA') && isa.length !== 104) {
+      throw new Error('Invalid EDI Data');
+    }
+    this.delimiters = new Delimiters(ediData);
+  }
 
   getDelimiters() {
     return this.delimiters;
   }
 
   buildSegment(segment: string) {
-    if (segment.startsWith('ISA')) {
-      this.delimiters = new Delimiters(segment);
-      return new IsaBuilder(segment, this.delimiters);
-    }
     if (!this.delimiters) {
       throw new Error('Please process the ISA segment first.');
     } else {
-      if (segment.startsWith('IEA')) {
+      if (segment.startsWith('ISA')) {
+        return new IsaBuilder(segment, this.delimiters);
+      } else if (segment.startsWith('IEA')) {
         return new IeaBuilder(segment, this.delimiters);
       } else if (segment.startsWith('GS')) {
         return new GsBuilder(segment, this.delimiters);
