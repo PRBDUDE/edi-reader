edd7f871857cbf189e78e9193169f6a353c1497f
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-14 14:29:41 -0500

    add D8 format handling in edi-info-dialog

--- src/app/feature/clean-edi-viewer/edi-info-dialog/edi-info-dialog.ts
+++ src/app/feature/clean-edi-viewer/edi-info-dialog/edi-info-dialog.ts
@@ -1,15 +1,25 @@
 import {Component, input} from '@angular/core';
 import {EdiBuilder} from '@edi/builders/edi-builder';
 import {DecimalPipe} from '@angular/common';
+import {D8DatePipe} from '@pipes/d8-date-pipe';
 
 @Component({
   selector: 'prb-edi-info-dialog',
   imports: [
-    DecimalPipe
+    DecimalPipe,
+    D8DatePipe
   ],
   templateUrl: './edi-info-dialog.html',
   styleUrl: './edi-info-dialog.scss',
 })
 export class EdiInfoDialog {
   segment = input<EdiBuilder>();
+
+  isLastElementD8(index: number) {
+    if (index === 0) return false;
+    const elements = this.segment()?.getElements();
+    return elements?.[index - 1].getElement() === 'D8';
+  }
+
+  protected readonly String = String;
 }
